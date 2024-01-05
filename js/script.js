300054d082cb90bcbb97792482265a71f05aa1d3
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-01-05 18:27:05 +0400

    section2-done

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let navigationJs1 = document.getElementById("navigationjs");
+let burgerjs1 = document.getElementById("burgerjs");
+
+burgerjs1.addEventListener("click",function(){
+    navigationJs1.classList.toggle("newnavigation");
+}) ;
